c8c63a9cab2c131ed33d1b9ebf06a3706a2e5d4a
Strawbernie <127209681+Strawbernie@users.noreply.github.com>
2024-03-12 10:19:42 +0100

    added maps, locationswitch and ARCamera

---
+++ Assets/ARMapSwitch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ARMapSwitch : MonoBehaviour
+{
+    public TextMeshProUGUI ButtonText;
+    bool mapOpen = false;
+    public GameObject LocationButtons;
+    public GameObject ARCamera;
+    public GameObject MapCamera;
+    public GameObject PlayerIndicator;
+
+    public void OnButtonPress()
+    {
+        if (!mapOpen)
+        {
+            mapOpen = true;
+            LocationButtons.SetActive(true);
+            ButtonText.text = "To AR";
+            MapCamera.SetActive(true);
+            ARCamera.SetActive(false);
+            PlayerIndicator.SetActive(true);
+        }
+        else if (mapOpen)
+        {
+            mapOpen = false;
+            LocationButtons.SetActive(false);
+            ButtonText.text = "To Map";
+            MapCamera.SetActive(false);
+            ARCamera.SetActive(true);
+            PlayerIndicator.SetActive(false);
+        }
+    }
+}
---
+++ Assets/GPSLocation/ButtonManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public float latitude;
+    private float cameraX;
+    public float longitude;
+    private float cameraY;
+    private float cameraZ = -10;
+    public Transform Camera1;
+    public GameObject Map;
+    public GameObject FalseMap;
+
+    void Start()
+    {
+        cameraX = latitude * 400000;
+        cameraY = longitude * 400000;
+    }
+
+    public void OnButtonPress()
+    {
+        Camera1.localPosition = new Vector3(cameraX, cameraY, cameraZ);
+        Map.SetActive(true);
+        FalseMap.SetActive(false);
+    }
+}
